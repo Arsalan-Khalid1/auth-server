54099c00af10604de3554e5148b31ecd01e2fd5d
Arsalan Khalid <arslankhalid889@gmail.com>
2021-09-04 02:39:17 -0700

    mvc refactoring and userScheme

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const express = require("express");
 const res = require("express/lib/response");
+const morgan = require("morgan");
+const authRoutes = require("./routes/auth");
 
 const port = process.env.PORT || 8000;
 
 const app = express();
 
+// app.use(morgan());
 app.use(express.json());
-
-app.get("/api/v1/sign_up", async (req, res) => {
-  res.json({
-    message: "api is working",
-  });
-});
+app.use("/api/v1/auth", authRoutes);
 
 app.listen(port, () => {
   console.log(`server is running at port ${port}`);
