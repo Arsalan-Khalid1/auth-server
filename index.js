11a07962b090b20906ffcbcb4ddbfa144c197d2b
Arsalan Khalid <arslankhalid889@gmail.com>
2021-09-04 02:57:06 -0700

    db connection

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require("express");
-const res = require("express/lib/response");
+const cors = require("cors");
+const mongoose = require("mongoose");
 const morgan = require("morgan");
+require("dotenv").config();
 const authRoutes = require("./routes/auth");
 
 const port = process.env.PORT || 8000;
 
 const app = express();
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB Connection Successfull"))
+  .catch((err) => console.log("DB Connection Failed, error => ", err));
 
-// app.use(morgan());
+app.use(morgan("dev"));
+if ((process.env.NODE_ENV = "development")) {
+  app.use(cors({ origin: `http://localhost:3000` }));
+}
 app.use(express.json());
 app.use("/api/v1/auth", authRoutes);
 
