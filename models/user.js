0ab035647c92768b01995b2414df032769455892
Arsalan Khalid <arslankhalid889@gmail.com>
2021-09-04 02:40:48 -0700

    mvc refactoring and userScheme fix

--- models/user.js
+++ models/user.js
@@ -65,7 +65,7 @@ userSchema.methods = {
   },
 
   makeSalt: function () {
-    return Math.round(new Date().valueOf() * Math.random()) + "";
+    return Match.round(new Date().valueOf() * Math.random()) + "";
   },
 };
 
